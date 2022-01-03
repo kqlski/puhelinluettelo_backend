5ec3feefaebc8f7584dd07e44cd7fb42a30c4046
Ahlström Kalle <hashtagkalski@gmail.com>
2022-01-03 19:44:57 +0200

    added Procfile for Heroku

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan((tokens,req,res)=>{
     const method = tokens.method(req,res)
