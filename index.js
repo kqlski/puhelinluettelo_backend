0b4dd1de1b55eb39176138db5d8722f9d25e699a
Ahlström Kalle <hashtagkalski@gmail.com>
2022-01-04 05:44:59 +0200

    teht. 3.22. + uniqueValidator fix

--- index.js
+++ index.js
@@ -10,110 +10,110 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan((tokens, req, res) => {
-    const method = tokens.method(req, res)
-    const postData = method === 'POST'
-        ? JSON.stringify(req.body)
-        : null
-    return [
-        method,
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        postData
-    ].join(' ')
+  const method = tokens.method(req, res)
+  const postData = method === 'POST'
+    ? JSON.stringify(req.body)
+    : null
+  return [
+    method,
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    postData
+  ].join(' ')
 }))
 
 //send the infopage to /info
 app.get('/info', (req, res) => {
-    Person.find({})
-    .then(persons=>{
-        res.send(`
+  Person.find({})
+    .then(persons => {
+      res.send(`
         <div>
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>
         </div>
         `)
     })
-    
+
 })
 //get all
 app.get('/api/persons', (req, res) => {
-    Person.find({})
-        .then((persons) => {
-            res.json(persons)
-        })
+  Person.find({})
+    .then((persons) => {
+      res.json(persons)
+    })
 
 })
 //get single with id
-app.get('/api/persons/:id', (req, res,next) => {
-    Person.findById(req.params.id)
-        .then(result => {
-            if (result)
-                res.json(result)
-            else
-                res.status(404).end()
-        }
-        )
-        .catch(error=>next(error))
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(result => {
+      if (result)
+        res.json(result)
+      else
+        res.status(404).end()
+    }
+    )
+    .catch(error => next(error))
 })
 
 //create a new person
-app.post('/api/persons', (req, res,next) => {
+app.post('/api/persons', (req, res, next) => {
 
-    const body = req.body
+  const body = req.body
 
-    console.log(body, typeof body);
-    console.log(body.name);
-    console.log(body.number);
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
-    .catch(error=>next(error))
+  console.log(body, typeof body)
+  console.log(body.name)
+  console.log(body.number)
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+    .catch(error => next(error))
 })
 //update information for a person
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
-    console.log(body);
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  const body = req.body
+  console.log(body)
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 //delete a person
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndDelete(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 const errorHandler = (error, request, response, next) => {
-    console.log(error);
-    console.log('name ',error.name);
+  console.log(error)
+  console.log('name ', error.name)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    if(error.name=='ValidationError'){
-        return response.status(400).send(error.message)
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).send(error.message)
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-if(process.argv.length<3 || process.argv.length===4) {
-    console.log('invalid arguments');
-    process.exit(1)
+if (process.argv.length < 3 || process.argv.length === 4) {
+  console.log('invalid arguments')
+  process.exit(1)
 }
 
 
@@ -10,39 +10,39 @@ const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 const url =
-    `mongodb+srv://kqlski:${password}@cluster.c2hx3.mongodb.net/phonebook?retryWrites=true&w=majority`
+  `mongodb+srv://kqlski:${password}@cluster.c2hx3.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const getPersons = () => {
-    Person.find({}).then(resp=>{
-        console.log('phonebook:')
-        resp.forEach(n=>console.log(n.name,n.number))
-        mongoose.connection.close()
-    })
+  Person.find({}).then(resp => {
+    console.log('phonebook:')
+    resp.forEach(n => console.log(n.name, n.number))
+    mongoose.connection.close()
+  })
 }
 
 const addPerson = () => {
 
 
-    const person = new Person({
-        name,
-        number
-    })
+  const person = new Person({
+    name,
+    number
+  })
 
-    person.save().then(res => {
-        console.log(`added ${name} number ${number} to phonebook`);
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 if (process.argv.length === 3) {
-    getPersons()
+  getPersons()
 } else if (process.argv.length > 4)
-    addPerson()
+  addPerson()
